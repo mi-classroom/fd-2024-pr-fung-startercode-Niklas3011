12c9d13ab2d718808b2301354f4e31d3b13d79c7
Niklas3011 <mail@niklas-voss.com>
2024-09-26 12:11:42 +0200

    Variante F5B: Dynamisch via Javascript.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
-import { slideshow } from './modules/slideshow.js';
+import { slideshow } from "./modules/slideshow.js";
+import { fetchAndDisplayWorks } from "./modules/content.js";
 
 /* Main
 ############################################################################ */
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   hljs.highlightAll();
   slideshow();
+  fetchAndDisplayWorks();
 });
---
+++ assets/js/modules/content.js
@@ -0,0 +1,52 @@
+function fetchAndDisplayWorks() {
+  console.log("hello");
+  const worksUrl = "http://0.0.0.0:4000/works.json";
+
+  const worksList = document.querySelector("[data-js-finished-works]");
+
+  fetch(worksUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((work) => {
+        const listItem = document.createElement("li");
+        listItem.classList.add("work-item");
+
+        const img = document.createElement("img");
+        img.src = work.image ? work.image : "path/to/default-image.jpg";
+        img.alt = `Image for ${work.title}`;
+        img.classList.add("work-image");
+
+        const workInfo = document.createElement("div");
+        workInfo.classList.add("work-info");
+
+        const title = document.createElement("h3");
+        title.textContent = work.title;
+
+        const authorInfo = document.createElement("p");
+        authorInfo.classList.add("work-author");
+        authorInfo.textContent = `${work.author}, ${work.type}, ${new Date(
+          work.date
+        ).toLocaleDateString("de-DE", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}`;
+
+        workInfo.appendChild(title);
+        workInfo.appendChild(authorInfo);
+        listItem.appendChild(img);
+        listItem.appendChild(workInfo);
+        worksList.appendChild(listItem);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}
+
+export { fetchAndDisplayWorks };
